4be0c7a56575cc96f9d42c4a4dc97e20d2b3dc74
Juliano Custodio <juliano.custodio@hotmail.com.br>
2018-12-01 13:11:00 -0200

    Implement TitleView

--- DemoTitleView/App.xaml.cs
+++ DemoTitleView/App.xaml.cs
@@ -11,7 +11,7 @@ namespace DemoTitleView
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
